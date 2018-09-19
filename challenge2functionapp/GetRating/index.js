34ee4dc9b56513b25bbf927890da4cbb40f18547
Daniel Rocha <dcrocha@gmail.com>
2018-09-19 11:10:12 +0530

    added some mongo code to GetRating

--- challenge2functionapp/GetRating/index.js
+++ challenge2functionapp/GetRating/index.js
@@ -1,33 +1,54 @@
-module.exports = function (context, req) {
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
-    context.log('JavaScript HTTP trigger function processed a request.');
+module.exports = async function (context, req) {
+  context.log('JavaScript HTTP trigger function processed a request.');
 
+  // Connection URL
+  const url = 'mongodb://challenge2:ALIe02eyXOBTDUgdYQw65YlWyJAbECu9Vshn6wjdjX8zderEFZyAwsH7KVWlDl7TwWSewBeaMWGKJecxpfsR6A==@challenge2.documents.azure.com:10255/?ssl=true';
+  
+  // Database Name
+  const dbName = 'challenge2-db';
 
+  // COllection
+  const collectionName = 'rating-collection';
+  
+  try {
+    // Use connect method to connect to the Server
+    /*
+    let client = await MongoClient.connect(url);
+    const db = client.db(dbName);
+    const col = db.collection('find');
 
-    if (req.query.productId || (req.body && req.body.productId)) {
+    // Insert a single document
+    //const r = await col.insertMany([{a:1}, {a:1}, {a:1}]);
+    //assert.equal(3, r.insertedCount);
 
-        context.res = {
+    // Get first two documents that match the query
+    const docs = await col.find({}).limit(2).toArray();
+    context.log('Length of the database is:' + docs.length);
+    //assert.equal(2, docs.length);
+    */
+    
+  } 
+  catch (err) {
+    console.log(err.stack);
+  }
 
-            // status: 200, /* Defaults to 200 */
+  if (client) {
+    client.close();
+  }
 
-            body: "The product name for your product id " + (req.query.productId || req.body.productId) + " is Starfruit Explosion."
-
-        };
-
-    }
-
-    else {
-
-        context.res = {
-
-            status: 400,
-
-            body: "Please pass a productId on the query string or in the request body"
-
-        };
-
-    }
-
-    context.done();
-
-};              
+  if (req.query.name || (req.body && req.body.name)) {
+      context.res = {
+          // status: 200, /* Defaults to 200 */
+          body: "Hello 2" + (req.query.name || req.body.name)
+      };
+  }
+  else {
+      context.res = {
+          status: 400,
+          body: "Please pass a name on the query string or in the request body"
+      };
+  }
+};
